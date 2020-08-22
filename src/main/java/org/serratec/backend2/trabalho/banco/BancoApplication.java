6aa400025a7c3fc438ae48b381405d162c0bc59a
BrGUILHERMEHenrique <guilhermehenriquersouza17@gmail.com>
2020-08-22 09:35:14 -0300

    adicionando esqueleto do projeto

---
+++ src/main/java/org/serratec/backend2/trabalho/banco/BancoApplication.java
@@ -0,0 +1,13 @@
+package org.serratec.backend2.trabalho.banco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/serratec/backend2/trabalho/banco/BancoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.serratec.backend2.trabalho.banco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
